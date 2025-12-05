d2a9b2429cfd84d40e7cb044c29a37f7c5719fed
Dryumin <fresh_idea@inbox.ru>
2025-12-05 09:11:46 +0300

    Initial Commit

---
+++ Whatsaswim/WhatsaswimApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsaswimApp.swift
+//  Whatsaswim
+//
+//  Created by Денис Сергеевич on 05.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatsaswimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
